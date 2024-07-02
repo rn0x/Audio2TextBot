b88103be05958b14e327b980010f0814e52a84ee
rn0x <rn0x.me@gmail.com>
2024-07-02 13:31:32 +0300

    setup project structure, update README.md

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
 # Audio2TextBot
+
 Audio2TextBot is a Telegram bot that facilitates audio and video file processing to convert them into text format using various pre-trained models.
+
+![VIEWS](https://komarev.com/ghpvc/?username=rn0x-audio2textbot&label=REPOSITORY+VIEWS&style=for-the-badge)
+
+## Installation
+
+To run the bot locally or on your server, follow these steps:
+
+1. **Clone the repository:**
+
+   ```bash
+   git clone https://github.com/rn0x/Audio2TextBot.git
+   cd Audio2TextBot
+   ```
+
+2. **Install dependencies:**
+
+   Make sure you have Node.js (version 14 or higher) installed.
+
+   ```bash
+   npm install
+   ```
+
+3. **Set up environment variables:**
+
+   Create a `.env` file in the root directory with the following variables:
+
+   ```plaintext
+   TELEGRAM_TOKEN=your_telegram_bot_token
+   WHISPER_MODEL=your_whisper_model_path
+   # WHISPER_LANGUAGE="auto"
+   WHISPER_LANGUAGE=your_whisper_language
+   ```
+
+**WHISPER_MODEL**
+   ```plaintext
+| Model     | Disk   | RAM     |
+|-----------|--------|---------|
+| tiny      |  75 MB | ~390 MB |
+| tiny.en   |  75 MB | ~390 MB |
+| base      | 142 MB | ~500 MB |
+| base.en   | 142 MB | ~500 MB |
+| small     | 466 MB | ~1.0 GB |
+| small.en  | 466 MB | ~1.0 GB |
+| medium    | 1.5 GB | ~2.6 GB |
+| medium.en | 1.5 GB | ~2.6 GB |
+| large-v1  | 2.9 GB | ~4.7 GB |
+| large     | 2.9 GB | ~4.7 GB |
+```
+
+4. **Initialize the SQLite database:**
+
+   The bot uses `better-sqlite3` for database operations. Ensure the database file `database.db` is created automatically when the bot starts.
+
+5. **Start the bot:**
+
+   Run the bot using `nodemon` for development or `node` for production:
+
+   ```bash
+   npm start
+   ```
+
+## Usage
+
+- Start the bot and send an audio or video file to convert it into text.
+- Use the `/users` command to fetch all users who have interacted with the bot.
+
+## Bot Features
+
+- Converts audio and video files to text.
+- Logs user interactions in a SQLite database.
+- Supports multiple media formats and languages for transcription.
+
+## Author
+
+- [Ryan Almalki](https://github.com/rn0x)
+
+## License
+
+This project is licensed under the MIT License.
---
+++ index.js
@@ -0,0 +1,422 @@
+// index.js
+import "dotenv/config";
+import { Telegraf } from 'telegraf';
+import "dotenv/config";
+import fs from 'fs';
+import fetch from 'node-fetch';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import Audio2TextJS from 'Audio2TextJS';
+import crypto from 'crypto';
+import sqlite3 from 'better-sqlite3';
+
+// YOUR_TELEGRAM_BOT_TOKEN
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
+const TIMEOUT = 50 * 60 * 1000;; // 50 minutes
+const bot = new Telegraf(TELEGRAM_TOKEN, {
+    handlerTimeout: TIMEOUT
+});
+
+/**
+ * Maximum length allowed for a Telegram message.
+ * @type {number}
+ */
+const MAX_MESSAGE_LENGTH = 4096;
+
+// Define __dirname using import.meta.url and path.dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const botInfo = await bot.telegram.getMe();
+
+const downloadsDir = path.join(__dirname, 'downloads');
+if (!fs.existsSync(downloadsDir)) {
+    fs.mkdirSync(downloadsDir);
+}
+
+// Initialize the Audio2TextJS
+const sttConverter = new Audio2TextJS({
+    threads: 4,
+    processors: 1,
+    duration: 0,
+    maxLen: 0,
+    outputJson: true,
+    outputTxt: true,
+    outputCsv: true
+});
+
+const db = new sqlite3('database.db');
+
+// Create tables if they don't exist
+db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        username TEXT,
+        first_name TEXT,
+        is_bot INTEGER,
+        chat_type TEXT,
+        language_code TEXT
+    );
+
+    CREATE TABLE IF NOT EXISTS requests (
+        id INTEGER PRIMARY KEY,
+        message_id INTEGER,
+        user_id INTEGER,
+        chat_id INTEGER,
+        date INTEGER,
+        file_id TEXT,
+        duration INTEGER,
+        mime_type TEXT,
+        file_size INTEGER,
+        file_name TEXT,
+        file_url TEXT,
+        file_path TEXT
+    );
+
+    CREATE TABLE IF NOT EXISTS hashes (
+        fileHash TEXT PRIMARY KEY,
+        chat_id INTEGER,
+        file_url TEXT,
+        textRAW TEXT
+    );
+`);
+
+// Function to execute SQL query with error handling
+const executeQuery = (query, params = []) => {
+    try {
+        const stmt = db.prepare(query);
+        return stmt.run(params);
+    } catch (error) {
+        console.error(`Error executing query: ${query}`, error.message);
+        throw error;
+    }
+};
+
+// Function to load users from database
+const loadUsers = () => {
+    try {
+        const query = 'SELECT * FROM users';
+        return db.prepare(query).all();
+    } catch (error) {
+        console.error('Error loading users:', error.message);
+        return [];
+    }
+};
+
+// Function to save user to database
+const saveUser = (user) => {
+    try {
+        const query = `
+            INSERT INTO users (id, username, first_name, is_bot, chat_type, language_code)
+            VALUES (?, ?, ?, ?, ?, ?)
+            ON CONFLICT(id) DO UPDATE SET username=excluded.username, first_name=excluded.first_name, is_bot=excluded.is_bot, chat_type=excluded.chat_type, language_code=excluded.language_code
+        `;
+        executeQuery(query, [
+            user.id,
+            user.username || '', // تأكد من عدم وجود قيمة فارغة لاسم المستخدم
+            user.first_name || '', // تأكد من عدم وجود قيمة فارغة للاسم الأول
+            user.is_bot || 0, // استخدام قيمة افتراضية في حالة عدم وجود قيمة
+            user.chat_type || '', // تأكد من عدم وجود قيمة فارغة لنوع المحادثة
+            user.language_code || '' // تأكد من عدم وجود قيمة فارغة لرمز اللغة
+        ]);
+    } catch (error) {
+        console.error('Error saving user:', error.message);
+    }
+};
+
+
+// Function to remove user from database
+const removeUser = (userId) => {
+    try {
+        const query = 'DELETE FROM users WHERE id = ?';
+        executeQuery(query, [userId]);
+    } catch (error) {
+        console.error('Error removing user:', error.message);
+    }
+};
+
+// Function to check if user exists in database
+const isUserInDatabase = (userId) => {
+    try {
+        const query = 'SELECT COUNT(*) as count FROM users WHERE id = ?';
+        const result = db.prepare(query).get(userId);
+        return result.count > 0;
+    } catch (error) {
+        console.error('Error checking user in database:', error.message);
+        return false;
+    }
+};
+
+// Function to log user details to database
+const dbUsers = (ctx) => {
+    try {
+        const { from } = ctx.message;
+        const userData = { id: from.id, username: from.username, first_name: from.first_name, is_bot: from.is_bot, chat_type: ctx.chat.type, language_code: from.language_code };
+
+        if (!isUserInDatabase(userData.id)) {
+            saveUser(userData);
+        }
+    } catch (error) {
+        console.error('Error saving user data:', error.message);
+    }
+};
+
+// Function to download file from URL and save locally
+const downloadFile = async (url, outputPath) => {
+    try {
+        const response = await fetch(url);
+        const buffer = await response.arrayBuffer();
+        await fs.promises.writeFile(outputPath, Buffer.from(buffer));
+        return outputPath;
+    } catch (error) {
+        console.error('Error downloading file:', error.message);
+        throw error;
+    }
+};
+
+// Function to calculate file hash
+const calculateFileHash = async (filePath) => {
+    try {
+        const algorithm = 'sha256';
+        const fileData = await fs.promises.readFile(filePath);
+        const hash = crypto.createHash(algorithm).update(fileData).digest('hex');
+        return hash;
+    } catch (error) {
+        console.error('Error calculating file hash:', error.message);
+        throw error;
+    }
+};
+
+// Function to log request details to database
+const logRequest = async (message, fileUrl, filePath) => {
+    try {
+        const { message_id, from, chat, date, audio, voice, video } = message;
+        const media = audio || voice || video || {};
+
+        const query = `
+            INSERT INTO requests (message_id, user_id, chat_id, date, file_id, duration, mime_type, file_size, file_name, file_url, file_path)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        `;
+
+        executeQuery(query, [message_id, from.id, chat.id, date, media.file_id, media.duration, media.mime_type, media.file_size, media.file_name || 'audio.ogg', fileUrl, filePath]);
+    } catch (error) {
+        console.error('Error logging request:', error.message);
+        throw error;
+    }
+};
+
+/**
+ * Handles audio and video messages sent to the bot
+ * @param {Object} ctx - The update context from Telegraf
+ * @param {Object} media - The media object from the message
+ */
+const handleMediaMessage = async (ctx, media) => {
+    try {
+        // Check if the media duration exceeds 10 minutes (600 seconds)
+        if (media.duration > 600) {
+            return ctx.reply('⚠️ The media file is too long. Please send a file that is less than 10 minutes.');
+        }
+
+        const fileLink = await ctx.telegram.getFileLink(media.file_id);
+        const inputFileUrl = fileLink.href;
+        const extname = (media.mime_type === 'video/mp4') ? 'mp4' : (media.mime_type === 'audio/ogg') ? 'ogg' : 'mp3'
+        const inputFilePath = path.join(downloadsDir, `${media.file_id}.${extname}`);
+
+        // Log the request details to requests.json
+        await logRequest(ctx.message, inputFileUrl, inputFilePath);
+
+        ctx.reply('✅ Your request has been logged and will be processed shortly.');
+    } catch (error) {
+        ctx.reply(`❌ An error occurred while processing the message:\n${error.message}`);
+    }
+};
+
+// Function to process media files recorded in requests
+const processMediaFiles = async () => {
+    try {
+        const query = 'SELECT * FROM requests';
+        const requests = db.prepare(query).all();
+
+        if (requests.length === 0) {
+            setTimeout(processMediaFiles, 10 * 1000); // Retry processing after 10 seconds
+            return;
+        }
+
+        for (const request of requests) {
+            const { chat_id, message_id, file_url, file_path } = request;
+
+            await downloadFile(file_url, file_path);
+
+            const fileHash = await calculateFileHash(file_path);
+
+            const queryHash = 'SELECT * FROM hashes WHERE fileHash = ?';
+            const existingHash = db.prepare(queryHash).get(fileHash);
+
+            if (existingHash) {
+                const message = `✅ Conversion completed successfully:\n\n${existingHash.textRAW}`
+                const messagetrun = truncateText(message);
+                await bot.telegram.sendMessage(chat_id, messagetrun, {
+                    reply_to_message_id: message_id
+                });
+                executeQuery('DELETE FROM requests WHERE id = ?', [request.id]);
+                if (fs.existsSync(file_path)) await fs.promises.unlink(file_path);
+                setTimeout(processMediaFiles, 10 * 1000);
+                return;
+            }
+
+            const result = await sttConverter.runWhisper(file_path, process.env.WHISPER_MODEL, process.env.WHISPER_LANGUAGE);
+
+            if (result.success) {
+                const message = `✅ Conversion completed successfully:\n\n${result.output[1].data}`
+                const messagetrun = truncateText(message);
+                await bot.telegram.sendMessage(chat_id, messagetrun, {
+                    reply_to_message_id: message_id
+                });
+                await bot.telegram.sendDocument(chat_id, { source: fs.readFileSync(`${file_path}.OUTPUT.wav.json`), filename: 'text.json' }, {
+                    reply_to_message_id: message_id
+                });
+                await bot.telegram.sendDocument(chat_id, { source: fs.readFileSync(`${file_path}.OUTPUT.wav.txt`), filename: 'text.txt' }, {
+                    reply_to_message_id: message_id
+                });
+                await bot.telegram.sendDocument(chat_id, { source: fs.readFileSync(`${file_path}.OUTPUT.wav.csv`), filename: 'text.csv' }, {
+                    reply_to_message_id: message_id
+                });
+            } else {
+                await bot.telegram.sendMessage(chat_id, `❌ Conversion failed:\n${result.message}`, {
+                    reply_to_message_id: message_id
+                });
+            }
+
+            const insertHashQuery = `
+                INSERT INTO hashes (fileHash, chat_id, file_url, textRAW)
+                VALUES (?, ?, ?, ?)
+            `;
+            executeQuery(insertHashQuery, [fileHash, chat_id, file_url || '', result.output[1].data || '']);
+
+            if (fs.existsSync(file_path)) await fs.promises.unlink(file_path);
+            if (fs.existsSync(`${file_path}.OUTPUT.wav`)) await fs.promises.unlink(`${file_path}.OUTPUT.wav`);
+            if (fs.existsSync(`${file_path}.OUTPUT.wav.json`)) await fs.promises.unlink(`${file_path}.OUTPUT.wav.json`);
+            if (fs.existsSync(`${file_path}.OUTPUT.wav.txt`)) await fs.promises.unlink(`${file_path}.OUTPUT.wav.txt`);
+            if (fs.existsSync(`${file_path}.OUTPUT.wav.csv`)) await fs.promises.unlink(`${file_path}.OUTPUT.wav.csv`);
+
+            executeQuery('DELETE FROM requests WHERE id = ?', [request.id]);
+        }
+
+        setTimeout(processMediaFiles, 10 * 1000);
+    } catch (error) {
+        console.error('Error processing media files:', error.message);
+        setTimeout(processMediaFiles, 10 * 1000);
+    }
+};
+
+/**
+ * Truncates the given text if it exceeds the maximum message length.
+ * @param {string} text - The text to truncate.
+ * @returns {string} The truncated text, if necessary.
+ */
+function truncateText(text) {
+    if (text.length > MAX_MESSAGE_LENGTH) {
+        const truncated = '... (truncated)';
+        return text.substring(0, MAX_MESSAGE_LENGTH - truncated.length) + truncated;
+    } else {
+        return text;
+    }
+}
+
+// Startup message
+const numUsers = loadUsers().length;
+const startupMessage = `
+🤖 **Bot Startup Information**
+📅 Current Time: ${new Date().toLocaleString()}
+🚀 Bot Status: Operational
+👥 Users: ${numUsers}
+🤖 Bot Username: @${botInfo.username}
+📜 Bot Description: audio processing and transcription. It supports converting audio files to text using various pre-trained models.
+
+🌟 Enjoy using the bot!
+`;
+
+// Print startup message
+console.log(startupMessage);
+
+// Listen for text commands
+bot.start(async (ctx) => {
+    dbUsers(ctx);
+
+    ctx.reply(`
+🎤 Welcome! Send me an audio or video file to convert it to text.\n\n💬 If you have any questions, please feel free to ask @f93ii.
+`);
+});
+
+// Handle the command to fetch all users in batches
+bot.command('users', async (ctx) => {
+    try {
+        const Users = loadUsers();
+        const batchSize = 10;
+
+        for (let i = 0; i < Users.length; i += batchSize) {
+            const batch = Users.slice(i, i + batchSize);
+
+            let message = `👥 Total number of users: ${Users.length}\n\n`;
+            for (let j = 0; j < batch.length; j++) {
+                const user = batch[j];
+                message += `
+${j + 1}️⃣ User ID: ${user.id}
+👤 Username: @${user.username || '-'}
+📛 First Name: ${user.first_name || '-'}
+💬 Chat Type: ${user.chat_type || '-'}
+🌐 Language Code: ${user.language_code || '-'}
+\n`;
+            }
+
+            await ctx.reply(message);
+        }
+    } catch (error) {
+        console.error('❌ Error fetching users:', error);
+        await ctx.reply(`❌ Error fetching users: ${error.message}`);
+    }
+});
+
+// Handle bot being deleted from the chat
+bot.on('my_chat_member', async (ctx) => {
+    const new_chat_member = ctx?.update?.my_chat_member?.new_chat_member;
+    const status = new_chat_member?.status;
+
+    if (status === 'left' || status === 'kicked') {
+        const chatId = ctx.chat.id; // Get the chat ID where the bot was deleted
+        removeUser(chatId);
+    }
+});
+
+// Listen for audio messages
+bot.on('audio', async (ctx) => {
+    dbUsers(ctx);
+    await handleMediaMessage(ctx, ctx.message.audio);
+});
+
+// Listen for voice messages
+bot.on('voice', async (ctx) => {
+    dbUsers(ctx);
+    await handleMediaMessage(ctx, ctx.message.voice);
+});
+
+// Listen for video messages
+bot.on('video', async (ctx) => {
+    dbUsers(ctx);
+    await handleMediaMessage(ctx, ctx.message.video);
+});
+
+// Handle errors
+bot.catch((err, ctx) => {
+    console.error(`❌ Error encountered in the bot: ${err}`);
+    ctx.reply(`❌ Sorry, something went wrong.`);
+});
+
+// Start processing media files from requests.json
+processMediaFiles();
+
+// Launch the bot
+bot.launch().then(() => {
+    console.log(`🚀 Bot @${botInfo.username} is running`);
+}).catch(err => {
+    console.error(`❌ Error launching bot: ${err}`);
+});
